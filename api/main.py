864778bd83fe38f93375ce1b5a4e16352969b553
mac <mac@MacBook-Air.local>
2022-01-11 18:33:44 +0800

    test nginx connect to flask with http proxy

--- api/main.py
+++ api/main.py
@@ -1,20 +1,20 @@
 #main.py
 from flask import Flask, jsonify, request
-from db import get_users, add_user
+# from db import get_users, add_user
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return "Hello world"
 
-@app.route('/users', methods=['POST', 'GET'])
-def users():
-    if request.method == 'POST':
-        user = request.get_json(force=True)
-        add_user(user)
-        return 'User added'
-    return get_users()
+# @app.route('/users', methods=['POST', 'GET'])
+# def users():
+#     if request.method == 'POST':
+#         user = request.get_json(force=True)
+#         add_user(user)
+#         return 'User added'
+#     return get_users()
 
 if __name__ == '__main__':
     app.run()
